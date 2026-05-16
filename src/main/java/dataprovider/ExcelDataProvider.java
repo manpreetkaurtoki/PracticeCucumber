3461457e805fc0167623ff72f6d5ff1098ebc18e
Manpreet <manpreetkaurtoki@gmail.com>
2026-05-16 15:35:11 -0400

    Print Excel Data

--- src/main/java/dataprovider/ExcelDataProvider.java
+++ src/main/java/dataprovider/ExcelDataProvider.java
@@ -6,26 +6,28 @@ import utils.ExcelUtils;
 
 public class ExcelDataProvider {
 	@DataProvider(name = "loginData")
-    public static Object[][] getLoginData() {
+	public static Object[][] getLoginData() {
 
-        String path = "src/test/resources/testdata/LoginData.xlsx";
+		String path = "src/test/resources/testdata/LoginData.xlsx";
 
-        ExcelUtils excel = new ExcelUtils(path, "Sheet1");
+		ExcelUtils excel = new ExcelUtils(path, "Sheet1");
 
-        int rows = excel.getRowCount();
-        int cols = excel.getColCount();
+		int rows = excel.getRowCount();
+		int cols = excel.getColCount();
 
-        Object[][] data = new Object[rows - 1][cols];
+		Object[][] data = new Object[rows - 1][cols];
+		String rowData = "";
+		for (int i = 1; i < rows; i++) {
 
-        for (int i = 1; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
 
-            for (int j = 0; j < cols; j++) {
+				data[i - 1][j] = excel.getCellData(i, j);
+				rowData += data[i - 1][j] + " ";
+				System.out.println(data[i - 1][j]);
+			}
+			System.out.println("Row-wise data : "+rowData);
+		}
 
-                data[i - 1][j] = excel.getCellData(i, j);
-               System.out.println( data[i - 1][j]);
-            }
-        }
-
-        return data;
-    }
+		return data;
+	}
 }
